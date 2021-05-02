507358f5919278c9494428ffda0182e454d14795
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-02 17:28:20 +0100

    Added BootStrap Cards for the library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
 import sanityClient from "../client";
+
 export default function Product() {
   const [productData, setProduct] = useState(null);
 
@@ -20,25 +22,15 @@ export default function Product() {
       <section className='container mx-auto'>
         <div className='grid md:grid-cols-4 lg:grid-cols-5 gap-7'>
           {productData &&
-            productData.map((product, index) => (
-              <article>
-                <a
-                  href={product.link}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <span
-                    className='block h-16 relative rounded shadow leading-snug bg-white border-l-4 border-yellow-700'
-                    key={index}
-                  >
-                    <span className='block relative h-full flex jsutify-end items-end pr-4 pb-4'>
-                      <h3 className='text-gray-800 textlg font-blog px-3 py-3 bg-white rounded'>
-                        {product.title}
-                      </h3>
-                    </span>
-                  </span>
-                </a>
-              </article>
+            productData.map((product) => (
+              <Card>
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Link href={product.link} target='_blank'>
+                    Download
+                  </Card.Link>
+                </Card.Body>
+              </Card>
             ))}
         </div>
       </section>
