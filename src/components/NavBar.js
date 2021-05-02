99f6e75f879503d3f854a63362661dbe7b5c1e88
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-02 22:09:52 +0100

    Changed href in navbar to React Link tag

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>Ingrid Telles de Abreu</Navbar.Brand>
+      <Navbar.Brand>Ingrid Telles de Abreu</Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='/'>Library</Nav.Link>
-          <Nav.Link href='/about'>About Me</Nav.Link>
+          <Nav.Link>
+            <Link to='/'>Library</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to='/about'>About</Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
