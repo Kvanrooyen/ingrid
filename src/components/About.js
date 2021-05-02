d41a6b6de885062af01745dc93549108b5a4c797
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-02 17:54:09 +0100

    Created about page with BootStrap

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { Col } from "react-bootstrap";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -27,24 +30,27 @@ export default function About() {
   if (!author) return <di>Loading...</di>;
 
   return (
-    <main className='relative'>
-      <div className='md: container mx-auto bg-gray-100 rounded-lg'>
-        <section className='shadow-2xl lg:flex p-10'>
-          <img
-            src={urlFor(author.authorImage).url()}
-            className='rounded w-32 h-32 lg:w-64 lg:h-64 mr-8'
-            alt={author.name}
-          />
-
-          <div className='px-4 lg:px-6 py-4 lg:py-6 prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto'>
-            <BlockContent
-              blocks={author.bio}
-              projectId='qddjgdan'
-              dataset='production'
+    <Container>
+      <Col>
+        <Row>
+          <Col xs={6} md={3}>
+            <img
+              src={urlFor(author.authorImage).url()}
+              className='rounded w-32 h-32 lg:w-64 lg:h-64 mt-14'
+              alt={author.name}
             />
-          </div>
-        </section>
-      </div>
-    </main>
+          </Col>
+          <Col xs={12} md={9}>
+            <div className='px-3 lg:px-6 py-3 lg:py-6 prose prose-sm sm:prose lg:prose-lg xl:prose-xl'>
+              <BlockContent
+                blocks={author.bio}
+                projectId='qddjgdan'
+                dataset='production'
+              />
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Container>
   );
 }
