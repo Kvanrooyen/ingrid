9a40da71440ac2b3aae4af422e81ff3311d223f1
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-03 21:43:50 +0100

    Added Welcome page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Library from "./components/Library";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route component={Library} path='/' exact />
+        <Route component={Welcome} path='/' exact />
+        <Route component={Library} path='/library' />
         <Route component={About} path='/about' />
       </Switch>
     </BrowserRouter>
