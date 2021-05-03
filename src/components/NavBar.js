6569b1b267dab0f167cb9f3e3fc6346675bdc7f8
Keagan van Rooyen <governor.keagan@outlook.com>
2021-05-03 21:43:12 +0100

    Updated to follow Ingrid's colour scheme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,28 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
+import "../index.css";
 
 export default function NavBar() {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand>Ingrid Telles de Abreu</Navbar.Brand>
+    <Navbar className='ingrid-nav' expand='lg'>
+      <Navbar.Brand>
+        <Link to='/' className='text-white'>
+          Ingrid Telles de Abreu
+        </Link>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
           <Nav.Link>
-            <Link to='/'>Library</Link>
+            <Link to='/library' className='text-white'>
+              Library
+            </Link>
           </Nav.Link>
           <Nav.Link>
-            <Link to='/about'>About</Link>
+            <Link to='/about' className='text-white'>
+              About
+            </Link>
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
